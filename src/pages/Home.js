0adc4bd899f3a4fbfe39ff96551cc232e3705356
poyapli <963973854@qq.com>
2021-10-01 00:18:32 +0800

    完成整体页面结构搭建，用 styled-components 控制样式

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home(){
+    return (
+        // 这是 React.Fragment 标签的简写
+        <>
+            <h1>Home</h1>
+        </>
+    )
+}
+
+export default Home;
