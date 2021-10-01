222655bbca3b10fa7c7dbb4c7ea87b5f1172308a
poyapli <963973854@qq.com>
2021-10-01 14:29:41 +0800

    完成登录按钮和注册按钮，登录信息和注册信息的显示。

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,21 +16,39 @@ const Logo = styled.img`
 const StyledLink = styled(NavLink)`
   color: #fff;
   margin-left: 30px;
+
   &.active {
     border-bottom: 2px solid #fff;
   }
 `
+const Login = styled.div`
+  margin-left: auto;
+`
+const Button = styled.button`
+  margin-left: 10px;
+`
 
-function Component(){
-    return (
-        <Header>
-            <Logo src={LogoUrl} alt=""/>
-            <nav>
-                <StyledLink to="/" activeClassName="active" exact>首页</StyledLink>
-                <StyledLink to="/history" activeClassName="active">上传历史</StyledLink>
-                <StyledLink to="/about" activeClassName="active">关于我</StyledLink>
-            </nav>
-        </Header>
-    )
+function Component() {
+  return (
+    <>
+    <Header>
+      <Logo src={LogoUrl} alt=""/>
+      <nav>
+        <StyledLink to="/" activeClassName="active" exact>首页</StyledLink>
+        <StyledLink to="/history" activeClassName="active">上传历史</StyledLink>
+        <StyledLink to="/about" activeClassName="active">关于我</StyledLink>
+      </nav>
+      <Login>
+        <Button>
+          <StyledLink to="/login" activeClassName="active">登录</StyledLink>
+        </Button>
+        <Button>
+          <StyledLink to="/register" activeClassName="active">注册</StyledLink>
+        </Button>
+      </Login>
+    </Header>
+    </>
+  )
 }
+
 export default Component;
