78f9dae7a450e0f9f552d6e22a95ff46a906ffbe
poyapli <963973854@qq.com>
2021-10-08 21:42:40 +0800

    上传结果展示

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -3,8 +3,24 @@ import {useStores} from '../stores';
 import {observer} from 'mobx-react';
 import {Upload, message} from 'antd';
 import {InboxOutlined} from '@ant-design/icons';
+import styled from 'styled-components';
 
 const {Dragger} = Upload;
+
+const Result = styled.div`
+  margin-top: 30px;
+  border:1px dashed #ccc;
+  padding: 20px;
+`
+
+const H1 = styled.h1`
+  margin: 20px 0;
+  text-align: center;
+`
+
+const Image = styled.img`
+  max-width: 300px;
+`
 
 const Component = observer(() => {
   const {ImageStore, UserStore} = useStores();
@@ -35,18 +51,36 @@ const Component = observer(() => {
         <p className="ant-upload-drag-icon">
           <InboxOutlined />
         </p>
-        <p className="ant-upload-text">Click or drag file to this area to upload</p>
+        <p className="ant-upload-text">点击或者拖拽图片到此处进行上传</p>
         <p className="ant-upload-hint">
-          Support for a single or bulk upload. Strictly prohibit from uploading company data or other
-          band files
+          支持单次或批量上传，严禁上传公司资料或其他类型文件
         </p>
       </Dragger>
-      <div>
-        <h1>上传结果</h1>
-        {
-          ImageStore.serverFile ? <div> { ImageStore.serverFile.attributes.url.attributes.url } </div> : null
-        }
-      </div>
+      {
+        ImageStore.serverFile ? <Result>
+          <H1>上传结果</H1>
+          <dl>
+            <dt>线上地址</dt>
+            <dd>
+              <a target="_blank" href={ ImageStore.serverFile.attributes.url.attributes.url }>{ ImageStore.serverFile.attributes.url.attributes.url }</a>
+            </dd>
+            <dt>文件名</dt>
+            <dd>{ImageStore.filename}</dd>
+            <dt>图片预览</dt>
+            <dd>
+              <Image src="{ ImageStore.serverFile.attributes.url.attributes.url }" alt=""/>
+            </dd>
+            <dt>更多尺寸</dt>
+            <dd>
+              <input type="" placeholder="最大宽度（可选）"/>
+              <input type="" placeholder="最大高度（可选）"/>
+            </dd>
+            <dd>
+
+            </dd>
+          </dl>
+        </Result> : null
+      }
     </div>
   )
 })
