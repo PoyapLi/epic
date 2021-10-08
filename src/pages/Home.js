1454f5a8e7ae1e277749a1abb7ac2d17f3213a08
poyapli <963973854@qq.com>
2021-10-08 17:27:36 +0800

    非受控表单与 useRef 实现简易上传

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import {observer} from "mobx-react";
 import {useStores} from "../stores";
+import Uploader from '../components/Uploader';
 
 const Home = observer (() => {
   const {UserStore} = useStores();
 
     return (
-        // 这是 React.Fragment 标签的简写
-        <>
-          {
-              UserStore.currentUser ? <>您好，{UserStore.currentUser.attributes.username} 阁下</> : '用户没有登录'
-          }
+      // 这是 React.Fragment 标签的简写
+      <>
+        {
+          UserStore.currentUser ? <>您好，{UserStore.currentUser.attributes.username} 阁下</> : '用户没有登录'
+        }
 
-        </>
+        <Uploader />
+
+      </>
     )
 })
 
